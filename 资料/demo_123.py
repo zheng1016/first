c5cfd473634e56c65c0256825ffee2f58ec9123d
‘zhangzheng’ <2064317177@qq.com>
2019-01-05 16:14:14 +0800

    ‘删除’

--- 资料/demo_123.py
+++
@@ -1,5 +0,0 @@
-# 输入三个值，输出三个值中的最大值
-a = int(input('请输入一个数字：'))
-b = int(input('请输入一个数字：'))
-c = int(input('请输入一个数字：'))
-print('最大值是:',(a if a > c else c)if a > b else (b if b > c else c))
--- 资料/百度百科/HTML_百度百科_files/snippetcacher.php
+++
@@ -1,2 +0,0 @@
-adsbybaidu_callback({"dpv":"21d79742b1d4fe22"}
-)
